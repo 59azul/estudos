59bce6865fb9f50c7d4f3702ba51a8905269374b
59azul <danilopaixao00@gmail.com>
2018-05-23 11:26:10 -0300

    Formatação calculadora e hello world usando wxWidgets

--- Calculadora.py
+++ Calculadora.py
@@ -1,40 +1,39 @@
-def Calculadora():
-    num1 = 0
-    num2 = 0
-    resultado = 0
-    operacao = 0
+def calculadora():
     num1 = input("Digite um numero: ")
-    if not Eh_int(num1):
+    if not eh_int(num1):
         return "Não é um número"
     num1 = int(num1)
     operacao = input("Digite a operação: ")
-    if not Op_valida(operacao):
+    if not op_valida(operacao):
         return "Operação inválida"
     num2 = input("Digite o segundo numero: ")
-    if not Eh_int(num2):
+    if not eh_int(num2):
         return "Não é um número"
     num2 = int(num2)
     if operacao == "+":
-        return (num1 + num2)
+        return num1 + num2
     elif operacao == "-":
-        return (num1 - num2)
+        return num1 - num2
     elif operacao == "*":
-        return (num1 * num2)
+        return num1 * num2
     elif operacao == "/":
-        return (float(num1) / float(num2))
+        return float(num1) / float(num2)
     return "Erro"
 
-def Eh_int(entrada):
+
+def eh_int(entrada):
     try:
         val = int(entrada)
         return True
     except ValueError:
         return False
 
-def Op_valida(entrada):
+
+def op_valida(entrada):
     if entrada == "+" or entrada == "-" or entrada == "*" or entrada == "/":
         return True
     else:
         return False
 
-print(Calculadora())
+
+print(calculadora())
